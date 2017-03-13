21ac98d9aa6ce4ad484ca069fdb1932a9015c59a
Dariusz Pawlukiewicz <Darek@MacBook-Pro-Dariusz.local>
2017-03-13 20:19:16 +0100

    added yo .NET Core + Nancy

---
+++ src/HomeModule.cs
@@ -0,0 +1,12 @@
+namespace gifty.Services.Users
+{
+    using Nancy;
+
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get("/", args => "Hello from Nancy running on CoreCLR");
+        }
+    }
+}
---
+++ src/Startup.cs
@@ -0,0 +1,13 @@
+namespace gifty.Services.Users
+{
+    using Microsoft.AspNetCore.Builder;
+    using Nancy.Owin;
+
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseOwin(x => x.UseNancy());
+        }
+    }
+}
